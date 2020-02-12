94ba139aff83d1ede4b7e997f43f9c43df818601
Orion Capell <orion.capell@gmail.com>
2020-02-12 13:01:19 -0800

    Input continued

--- input_.py
+++ input_.py
@@ -1,11 +1,17 @@
 import time
 import requests
+from twilio.rest import Client
 #from bs4 import BeautifulSoup4
 
-# Gets page and prints HTML, entirely for testing purposes at the moment
+# Gets page
 page = requests.get("https://xkcd.com/")
+print(page)
 
 # Getting user input
 userNum = str(input("What is your phone number? "))
 twilioNum = str(input("What is your Twilio number? "))
 twilioAuth = str(input("What is your Twilio authentification token? "))
+twilioSID = str(input("What is your Twilio SID? "))
+
+# Actual user
+client = Client(twilioSID, twilioAuth)
